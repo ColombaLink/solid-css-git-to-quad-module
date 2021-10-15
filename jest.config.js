f5a076783e6f4eb0593389cb9e218f30054d3aef
fuubi <fuubi@protonmail.ch>
2021-10-15 09:30:05 +0200

    Chore: init setup

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '/test/(unit|integration)/.*\\.test\\.ts$',
+  moduleFileExtensions: [
+    'ts',
+    'js',
+  ],
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/test/util/SetupTests.ts'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/dist/',
+    '/node_modules/',
+    '/test/',
+  ],
+  moduleNameMapper: {
+    '^jose/(.*)$': '<rootDir>/node_modules/jose/dist/node/cjs/$1',
+  },
+  // Slower machines had problems calling the WebSocket integration callbacks on time
+  testTimeout: 60_000,
+};
